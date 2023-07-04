dd3ae195bfd53b3d490372d567d6e87d47d18651
Crispas Makanani <crispasmakanani@gmail.com>
2023-07-04 10:27:01 +0200

    Creating a generic data fetching hook

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -4,13 +4,13 @@ import useGames from '../hooks/useGames';
 import GameCard from './GameCard';
 
 const GameGrid = () => {
-    const {games, error} = useGames()
+    const {data, error} = useGames()
 
   return (
     <>
         {error && <Text>{error}</Text>}
       <SimpleGrid columns={{sm: 1, md: 2, lg: 3,xl: 5}}>
-        {games.map((game) => (
+        {data.map((game) => (
           <GameCard key={game.id} game={game}/>
         ))}
       </SimpleGrid>
--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,34 +1,10 @@
-import { useEffect, useState } from "react";
-import apiClient from "../services/api-client";
-import { CanceledError } from "axios";
+import useData from "./useData";
 
-interface Genre {
+
+ export interface Genre {
     id: number;
     name: string
 }
 
-interface FetchGamesResponse {
-    count:number;
-    results: Genre[];
-}
-
-const useGenres =()=>{
-    const [genres, setGenre] = useState<Genre[]>([])
-    const [error, setError] = useState('')
-
-    useEffect(()=>{
-        const controller = new AbortController();
-
-
-        apiClient.get<FetchGamesResponse>('/genres', {signal: controller.signal})
-        .then(res => setGenre(res.data.results))
-        .catch(err  => {
-            if(err instanceof CanceledError) return;
-            setError(err.message)})
-
-        return () => controller.abort()
-    },[])
-
-    return {genres, error}
-}
+const useGenres =()=> useData<Genre>('/genres')
 export default useGenres;
